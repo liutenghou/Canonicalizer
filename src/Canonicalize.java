590ac2c176028300e7d374cd10511091b479db64
Leo Teng Liu <liutenghou@gmail.com>
2017-02-05 18:38:53 -0800

    interactive mode

--- src/Canonicalize.java
+++ src/Canonicalize.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Canonicalize {
 
@@ -9,8 +9,48 @@ public class Canonicalize {
 //		System.out.println("Read Input From File? (Y/N)?  ");
 //		
 //		System.out.println("Read Input From Console? (Y/N)? ");
+		Scanner scan = new Scanner(System.in);
 		
-		String input = "3x^2 + 3.5xy - 3.3y = y^2 - 555xy + y + x^2";
+		String modeSelected = "";
+		while(true){
+			System.out.print("File(F) or Interactive(I) Mode>");
+			String modeChoice = scan.nextLine();
+			String fileRegex = "[Ff](ile)?";
+			String interactiveRegex = "[Ii](nteractive)?";
+			if(modeChoice.matches(fileRegex)){
+				//System.out.println("File Mode");
+				modeSelected = "F";
+				break;
+			}else if(modeChoice.matches(interactiveRegex)){
+				//System.out.println("Interactive Mode");
+				modeSelected = "I";
+				break;
+			}else{
+				System.out.println("Invalid Selection");
+			}
+		}
+		
+		if(modeSelected.matches("F")){ //open file
+			
+		}else if(modeSelected.matches("I")){
+			while(true){
+				System.out.print("Equation>");
+				String input = scan.nextLine();
+				parseAndPrintInput(input);
+				
+			}
+			
+		}else{
+			System.out.println("ERROR: modeSelected");
+		}
+		
+		scan.close();
+		
+	}
+	
+	private static void parseAndPrintInput(String input){
+
+		//input = "3x^2 + 3.5xy - 3.3y = y^2 - 555xy + y + x^2";
 		
 		String[] splitEquation = input.split("=");
 		
@@ -38,19 +78,18 @@ public class Canonicalize {
 		createTerms(rightTerms, rightExpression);
 		
 		//Test Code
-		for(Term t: leftTerms){
-			System.out.print("coef: " + t.getCoefficient() + " variable: " + t.getVariable() + ". \n");
-		}
-		System.out.println("");
-		for(Term t: rightTerms){
-			System.out.print("coef: " + t.getCoefficient() + " variable: " + t.getVariable() + ". \n");
-		}
-		System.out.println("");
+//		for(Term t: leftTerms){
+//			System.out.print("coef: " + t.getCoefficient() + " variable: " + t.getVariable() + ". \n");
+//		}
+//		System.out.println("");
+//		for(Term t: rightTerms){
+//			System.out.print("coef: " + t.getCoefficient() + " variable: " + t.getVariable() + ". \n");
+//		}
+//		System.out.println("");
 		
 		//combine left and right
 		ArrayList<Term> ct = combineTerms(leftTerms, rightTerms);
 		printOutput(ct);
-		
 		
 	}
 	
@@ -63,6 +102,7 @@ public class Canonicalize {
 	        return String.format("%s",d);
 	}
 	
+	//prints the final output
 	private static void printOutput(ArrayList<Term> ct){
 		//print out final result in clean format
 		for(int i=0; i<ct.size(); i++){
@@ -73,7 +113,7 @@ public class Canonicalize {
 			//format
 			if(t.getCoefficient() == 1.0 || t.getCoefficient() == -1.0){
 				coef = "";
-			}else if(t.getCoefficient() < 0){
+			}else if(t.getCoefficient() < 0 && i != 0){
 				coef = cleanDoubleFormat(t.getCoefficient()*-1);
 			}else{
 				coef = cleanDoubleFormat(t.getCoefficient());
@@ -91,7 +131,7 @@ public class Canonicalize {
 			}
 
 		}
-		System.out.print("= 0");
+		System.out.print("= 0\n");
 	}
 	
 	//TODO: assume that coefficients only appear once per side, error if not
@@ -115,8 +155,14 @@ public class Canonicalize {
 					if(combinedCoef < 0){
 						operator = "-";
 						combinedCoef = combinedCoef*(-1);
-					}else{
+					}else if(combinedCoef > 0){
 						operator = "+";
+					}else if(combinedCoef == 0){ //skip creating new term, just delete
+						//remove from leftTerm and rightTerm
+						lt.remove(i);
+						i--;
+						rt.remove(j);
+						continue;
 					}
 					
 					String combinedVariable = lt.get(i).getVariable();
